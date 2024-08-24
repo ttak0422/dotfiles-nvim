a297b2d527988b43591453df941dc07ac449e1b8
ttak0422 <ttak0422@gmail.com>
2024-08-25 00:50:18 +0900

    fix(leap): keymap

--- fnl/leap.fnl
+++ fnl/leap.fnl
@@ -1,2 +1,5 @@
-(let [M (require :leap)]
-  (M.create_default_mappings))
+; (let [M (require :leap)]
+;   (M.create_default_mappings))
+(each [_ K (ipairs [[[:n :x :o] :s "<Plug>(leap-forward)" "Leap forward"]
+                    [[:n :x :o] :S "<Plug>(leap-backward)" "Leap backward"]])]
+  (vim.keymap.set (. K 1) (. K 2) (. K 3)))
--- lua/autogen/leap.lua
+++ lua/autogen/leap.lua
@@ -1,3 +1,5 @@
 -- [nfnl] Compiled from fnl/leap.fnl by https://github.com/Olical/nfnl, do not edit.
-local M = require("leap")
-return M.create_default_mappings()
+for _, K in ipairs({{{"n", "x", "o"}, "s", "<Plug>(leap-forward)", "Leap forward"}, {{"n", "x", "o"}, "S", "<Plug>(leap-backward)", "Leap backward"}}) do
+  vim.keymap.set(K[1], K[2], K[3])
+end
+return nil
