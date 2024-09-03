1a56842b0fb4d50de20e3dbdc4711fe2389f36c6
ttak0422 <ttak0422@gmail.com>
2024-09-04 01:54:21 +0900

    chore(sonic): add snippet

---
+++ tmpl/sonic/lua/base-config-local.lua
@@ -0,0 +1,9 @@
+-- shared ----------------------------------------------------------------------
+
+-- environment variables
+-- e.g. vim.env.FOO = "foo"
+
+-- `FILE_NAME` replace to filename
+vim.g.sonic_license_header = nil
+
+--------------------------------------------------------------------------------
