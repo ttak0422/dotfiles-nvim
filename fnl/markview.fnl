505f46b5daa79d5a991966df915c7eb76563860e
ttak0422 <ttak0422@gmail.com>
2024-09-05 01:05:41 +0900

    chore(markview): modify concealcursor

--- fnl/markview.fnl
+++ fnl/markview.fnl
@@ -1,7 +1,7 @@
 (let [M (require :markview)
-      modes [:n :no :c]
-      hybrid_modes [:n]
+      modes [:n :i :no :c]
+      hybrid_modes [:i]
       callbacks {:on_enable (fn [_ win]
                               (tset (. vim.wo win) :conceallevel 3)
-                              (tset (. vim.wo win) :concealcursor :c))}]
+                              (tset (. vim.wo win) :concealcursor :nc))}]
   (M.setup {: modes : hybrid_modes : callbacks}))
--- lua/autogen/markview.lua
+++ lua/autogen/markview.lua
@@ -1,11 +1,11 @@
 -- [nfnl] Compiled from fnl/markview.fnl by https://github.com/Olical/nfnl, do not edit.
 local M = require("markview")
-local modes = {"n", "no", "c"}
-local hybrid_modes = {"n"}
+local modes = {"n", "i", "no", "c"}
+local hybrid_modes = {"i"}
 local callbacks
 local function _1_(_, win)
   vim.wo[win]["conceallevel"] = 3
-  vim.wo[win]["concealcursor"] = "c"
+  vim.wo[win]["concealcursor"] = "nc"
   return nil
 end
 callbacks = {on_enable = _1_}
