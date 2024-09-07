27534b0d21ee84f34a80ec2ac114baa2347e1a72
ttak0422 <ttak0422@gmail.com>
2024-09-07 20:26:28 +0900

    style(statuscol): modify

--- fnl/statuscol.fnl
+++ fnl/statuscol.fnl
@@ -1,10 +1,12 @@
-(let [opts {:foldcolumn :1 :signcolumn :yes :number true :foldenable true}]
-  (each [k v (pairs opts)]
-    (tset vim.o k v)))
+(each [k v (pairs {:foldcolumn :1
+                   :signcolumn :yes
+                   :number true
+                   :foldenable true})]
+  (tset vim.o k v))
 
-(let [M (setmetatable {:builtin (require :statuscol.builtin)}
-                      {:__index (require :statuscol)})
+(let [M (require :statuscol)
+      builtin (require :statuscol.builtin)
       segments [{:text ["%s"] :maxwidth 2 :click "v:lua.ScSa"}
-                {:text [M.builtin.lnumfunc] :click "v:lua.ScLa"}
-                {:text [" " M.builtin.foldfunc " "] :click "v:lua.ScFa"}]]
+                {:text [builtin.lnumfunc] :click "v:lua.ScLa"}
+                {:text [" " builtin.foldfunc " "] :click "v:lua.ScFa"}]]
   (M.setup {:setopt true :relculright false : segments}))
--- lua/autogen/statuscol.lua
+++ lua/autogen/statuscol.lua
@@ -1,10 +1,8 @@
 -- [nfnl] Compiled from fnl/statuscol.fnl by https://github.com/Olical/nfnl, do not edit.
-do
-  local opts = {foldcolumn = "1", signcolumn = "yes", number = true, foldenable = true}
-  for k, v in pairs(opts) do
-    vim.o[k] = v
-  end
+for k, v in pairs({foldcolumn = "1", signcolumn = "yes", number = true, foldenable = true}) do
+  vim.o[k] = v
 end
-local M = setmetatable({builtin = require("statuscol.builtin")}, {__index = require("statuscol")})
-local segments = {{text = {"%s"}, maxwidth = 2, click = "v:lua.ScSa"}, {text = {M.builtin.lnumfunc}, click = "v:lua.ScLa"}, {text = {" ", M.builtin.foldfunc, " "}, click = "v:lua.ScFa"}}
+local M = require("statuscol")
+local builtin = require("statuscol.builtin")
+local segments = {{text = {"%s"}, maxwidth = 2, click = "v:lua.ScSa"}, {text = {builtin.lnumfunc}, click = "v:lua.ScLa"}, {text = {" ", builtin.foldfunc, " "}, click = "v:lua.ScFa"}}
 return M.setup({setopt = true, segments = segments, relculright = false})
