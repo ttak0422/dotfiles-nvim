ed499121a5052eaec96b96797886453320640260
ttak0422 <ttak0422@gmail.com>
2024-10-10 02:06:49 +0900

    chore(bufferline): modify icon

--- fnl/bufferline.fnl
+++ fnl/bufferline.fnl
@@ -1,5 +1,7 @@
 (let [M (require :bufferline)
-      options {:modified_icon "[+]"
+      options {
+      :indicator {:icon "▊"}
+      :modified_icon ""
                :show_buffer_icons false
                :show_buffer_close_icons false
                :show_close_icon false
--- lua/autogen/bufferline.lua
+++ lua/autogen/bufferline.lua
@@ -1,4 +1,4 @@
 -- [nfnl] Compiled from fnl/bufferline.fnl by https://github.com/Olical/nfnl, do not edit.
 local M = require("bufferline")
-local options = {modified_icon = "[+]", left_trunc_marker = "\239\132\132", right_trunc_marker = "\239\132\133", show_buffer_close_icons = false, show_buffer_icons = false, show_close_icon = false}
+local options = {indicator = {icon = "\226\150\138"}, modified_icon = "\239\145\132", left_trunc_marker = "\239\132\132", right_trunc_marker = "\239\132\133", show_buffer_close_icons = false, show_buffer_icons = false, show_close_icon = false}
 return M.setup({options = options})
