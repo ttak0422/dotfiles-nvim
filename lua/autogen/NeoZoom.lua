de397ebdc85021f00915ae45893b00e3bc896229
ttak0422 <ttak0422@gmail.com>
2024-10-25 02:05:30 +0900

    chore(NeoZoom): fixup

--- lua/autogen/NeoZoom.lua
+++ lua/autogen/NeoZoom.lua
@@ -3,4 +3,4 @@ local function _1_()
   vim.wo.wrap = true
   return nil
 end
-return require("neo-zoom").setup({"popup", {"enabled", true}, "exclude_buftypes", {"terminal"}, "winopts", {offset = {width = 150, height = 0.85}, border = "none"}, "presets", {{filetypes = {"dapui_.*", "dap-repl"}, winopts = {offset = {top = 0.02, left = 0.26, width = 0.74, height = 0.25}}}, {filetypes = {"markdown"}, callbacks = {_1_}}}})
+return require("neo-zoom").setup({popup = {enabled = true}, exclude_buftypes = {"terminal"}, winopts = {offset = {width = 150, height = 0.85}, border = "none"}, presets = {{filetypes = {"dapui_.*", "dap-repl"}, winopts = {offset = {top = 0.02, left = 0.26, width = 0.74, height = 0.45}}}, {filetypes = {"markdown"}, callbacks = {_1_}}}})
