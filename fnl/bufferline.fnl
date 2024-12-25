bdedf96f523ee5d6e5e8469a20b27b92d9ed468c
ttak0422 <ttak0422@gmail.com>
2024-12-25 13:08:42 +0900

    chore(bufferline): modify highlights

--- fnl/bufferline.fnl
+++ fnl/bufferline.fnl
@@ -1,10 +1,28 @@
 (let [M (require :bufferline)
-      options {
-      :indicator {:icon "▊"}
-      :modified_icon ""
+      bg {:attribute :bg :highlight :Normal}
+      fg {:attribute :fg :highlight :Normal}
+      rev_bg {:attribute :fg :highlight :Normal}
+      rev_fg {:attribute :bg :highlight :Normal}
+      normal {: bg : fg}
+      active {:bg rev_bg :fg rev_fg}
+      highlights {:fill normal
+                  :background normal
+                  :tab normal
+                  :tab_selected active
+                  :buffer_visible {: bg : fg :bold true}
+                  :buffer_selected {:bg rev_bg
+                                    :fg rev_fg
+                                    :bold true
+                                    :italic true}
+                  :modified normal
+                  :modified_visible normal
+                  :modified_selected active}
+      options {:indicator {:icon "" :style :none}
+               :separator_style ["" ""]
+               :modified_icon ""
                :show_buffer_icons false
                :show_buffer_close_icons false
                :show_close_icon false
                :left_trunc_marker ""
                :right_trunc_marker ""}]
-  (M.setup {: options}))
+  (M.setup {: options : highlights}))
--- lua/autogen/bufferline.lua
+++ lua/autogen/bufferline.lua
@@ -1,4 +1,11 @@
 -- [nfnl] Compiled from fnl/bufferline.fnl by https://github.com/Olical/nfnl, do not edit.
 local M = require("bufferline")
-local options = {indicator = {icon = "\226\150\138"}, modified_icon = "\239\145\132", left_trunc_marker = "\239\132\132", right_trunc_marker = "\239\132\133", show_buffer_close_icons = false, show_buffer_icons = false, show_close_icon = false}
-return M.setup({options = options})
+local bg = {attribute = "bg", highlight = "Normal"}
+local fg = {attribute = "fg", highlight = "Normal"}
+local rev_bg = {attribute = "fg", highlight = "Normal"}
+local rev_fg = {attribute = "bg", highlight = "Normal"}
+local normal = {bg = bg, fg = fg}
+local active = {bg = rev_bg, fg = rev_fg}
+local highlights = {fill = normal, background = normal, tab = normal, tab_selected = active, buffer_visible = {bg = bg, fg = fg, bold = true}, buffer_selected = {bg = rev_bg, fg = rev_fg, bold = true, italic = true}, modified = normal, modified_visible = normal, modified_selected = active}
+local options = {indicator = {icon = "", style = "none"}, separator_style = {"", ""}, modified_icon = "\239\145\132", left_trunc_marker = "\239\132\132", right_trunc_marker = "\239\132\133", show_buffer_close_icons = false, show_buffer_icons = false, show_close_icon = false}
+return M.setup({options = options, highlights = highlights})
