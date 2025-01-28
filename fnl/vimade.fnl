922806d115495bf6cfe28aa1c10982d24a503cd2
ttak0422 <ttak0422@gmail.com>
2025-01-28 23:38:27 +0900

    chore(vimade): modify

--- fnl/vimade.fnl
+++ fnl/vimade.fnl
@@ -1,4 +1,5 @@
 ((. (require :vimade) :setup) {:blocklist {:default {:buf_opts {:buftype [:prompt
                                                                           :terminal]}
                                                      :win_config {:relative true}}}
+                               :fadelevel 0.5
                                :recipe {:minimalist {:animate false}}})
--- lua/autogen/vimade.lua
+++ lua/autogen/vimade.lua
@@ -1,2 +1,2 @@
 -- [nfnl] Compiled from fnl/vimade.fnl by https://github.com/Olical/nfnl, do not edit.
-return require("vimade").setup({blocklist = {default = {buf_opts = {buftype = {"prompt", "terminal"}}, win_config = {relative = true}}}, recipe = {minimalist = {animate = false}}})
+return require("vimade").setup({blocklist = {default = {buf_opts = {buftype = {"prompt", "terminal"}}, win_config = {relative = true}}}, fadelevel = 0.5, recipe = {minimalist = {animate = false}}})
