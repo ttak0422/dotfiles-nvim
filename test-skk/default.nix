94aa4f91a28f01d9ec91aa2cf4e6c8e89e5b1713
ttak0422 <ttak0422@gmail.com>
2025-01-30 01:50:44 +0900

    test: lazy

--- test-skk/default.nix
+++ test-skk/default.nix
@@ -1,14 +1,13 @@
 { pkgs, ... }:
 {
-  eager = with pkgs.vimPlugins; {
-    denops.package = denops-vim;
-    skk.package = skkeleton;
-  };
-  extraConfig = ''
-    vim.cmd([=[
-          let g:denops#deno = '${pkgs.deno}/bin/deno'
-          let g:denops#server#deno_args = ['-q', '--no-lock', '-A', '--unstable-kv']
-
+  lazy = with pkgs.vimPlugins; {
+    skk = {
+      depends = [ denops-vim ];
+      package = skkeleton;
+      hooks.events = [ "InsertEnter" ];
+      postConfig = {
+        language = "vim";
+        code = ''
           call skkeleton#config({
                 \   'sources': [ 'skk_server' ],
                 \   'globalDictionaries': ['${pkgs.skk-dict}/SKK-JISYO.L'],
@@ -21,6 +20,15 @@
           imap <C-j> <Plug>(skkeleton-enable)
           cmap <C-j> <Plug>(skkeleton-enable)
           tmap <C-j> <Plug>(skkeleton-enable)
+
+        '';
+      };
+    };
+  };
+  extraConfig = ''
+    vim.cmd([=[
+        let g:denops#deno = '${pkgs.deno}/bin/deno'
+        let g:denops#server#deno_args = ['-q', '--no-lock', '-A', '--unstable-kv']
     ]=])
   '';
 }
