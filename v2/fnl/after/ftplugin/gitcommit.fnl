a5c9e43b1fcf2e6bd1f0229b9b93548fb9776430
ttak0422 <ttak0422@gmail.com>
2025-05-18 10:23:02 +0900

    wip

--- v2/fnl/after/ftplugin/gitcommit.fnl
+++ v2/fnl/after/ftplugin/gitcommit.fnl
@@ -1,1 +1,2 @@
 (vim.opt_local.iskeyword:append "-")
+(set vim.bo.bufhidden :wipe)
--- v2/lua/autogen/after/ftplugin/gitcommit.lua
+++ v2/lua/autogen/after/ftplugin/gitcommit.lua
@@ -1,2 +1,4 @@
 -- [nfnl] Compiled from v2/fnl/after/ftplugin/gitcommit.fnl by https://github.com/Olical/nfnl, do not edit.
-return vim.opt_local.iskeyword:append("-")
+vim.opt_local.iskeyword:append("-")
+vim.bo.bufhidden = "wipe"
+return nil
