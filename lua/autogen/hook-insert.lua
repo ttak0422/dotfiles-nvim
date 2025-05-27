7c4fea3dbbc912e68bb7eb64b748bdc219c35d1a
ttak0422 <ttak0422@gmail.com>
2025-05-27 15:40:29 +0900

    refactor: remove orphan

--- lua/autogen/hook-insert.lua
+++
@@ -1,2 +0,0 @@
--- [nfnl] Compiled from full/fnl/hook-insert.fnl by https://github.com/Olical/nfnl, do not edit.
-return vim.cmd("inoremap <expr> <C-Space> '<C-n>'")
--- lua/autogen/treesj.lua
+++
@@ -1,3 +0,0 @@
--- [nfnl] Compiled from full/fnl/treesj.fnl by https://github.com/Olical/nfnl, do not edit.
-local M = require("treesj")
-return M.setup({use_default_keymaps = false})
