4a8cd6d3457a954180a68d47b9ec2c66e36f3a1b
ttak0422 <ttak0422@gmail.com>
2025-05-29 19:18:28 +0900

    chore(v2): register buffer-plugins keymaps

---
+++ v2/fnl/buffer-plugins.fnl
@@ -0,0 +1,6 @@
+(let [opts {:noremap true :silent true}
+      desc (fn [d] {:noremap true :silent true :desc d})]
+  (each [_ k (ipairs [[:<Leader>U
+                       :<Cmd>UndotreeToggle<CR>
+                       (desc " undotree")]])]
+    (vim.keymap.set :n (. k 1) (. k 2) (or (. k 3) opts))))
--- v2/lua/autogen/buffer-plugins.lua
+++ v2/lua/autogen/buffer-plugins.lua
@@ -1,2 +1,11 @@
 -- [nfnl] v2/fnl/buffer-plugins.fnl
-
+local opts = {noremap = true, silent = true}
+local desc
+local function _1_(d)
+  return {noremap = true, silent = true, desc = d}
+end
+desc = _1_
+for _, k in ipairs({{"<Leader>U", "<Cmd>UndotreeToggle<CR>", desc("\239\136\132 undotree")}}) do
+  vim.keymap.set("n", k[1], k[2], (k[3] or opts))
+end
+return nil
