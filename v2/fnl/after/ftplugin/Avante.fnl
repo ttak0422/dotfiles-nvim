341007a9a375038171bd519f94e2971e4fed5b0b
ttak0422 <ttak0422@gmail.com>
2025-07-22 23:30:48 +0900

    feat(avante): make avante buffer read-only
    Disable modification keymaps in avante (markdown preview) buffers to prevent accidental editing.

--- v2/fnl/after/ftplugin/Avante.fnl
+++ v2/fnl/after/ftplugin/Avante.fnl
@@ -1,1 +1,5 @@
 (set vim.opt_local.conceallevel 2)
+
+(let [opts {:buffer true :silent true}]
+  (each [_ key (ipairs [:i :a :o :A :I :O :s :S :c :C])]
+    (vim.keymap.set :n key :<Nop> opts)))
--- v2/lua/autogen/after/ftplugin/Avante.lua
+++ v2/lua/autogen/after/ftplugin/Avante.lua
@@ -1,3 +1,7 @@
 -- [nfnl] v2/fnl/after/ftplugin/Avante.fnl
 vim.opt_local.conceallevel = 2
+local opts = {buffer = true, silent = true}
+for _, key in ipairs({"i", "a", "o", "A", "I", "O", "s", "S", "c", "C"}) do
+  vim.keymap.set("n", key, "<Nop>", opts)
+end
 return nil
