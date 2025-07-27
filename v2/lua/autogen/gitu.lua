e205d60705f83a17f4fa5bb552a3ee9ba6a0571d
ttak0422 <ttak0422@gmail.com>
2025-07-27 19:19:02 +0900

    feat(gitu): add git repository check
    Adds a check to ensure that the current directory is a git repository before opening the gitu terminal.

--- v2/lua/autogen/gitu.lua
+++ v2/lua/autogen/gitu.lua
@@ -1,40 +1,44 @@
 -- [nfnl] v2/fnl/gitu.fnl
 local function gitu()
-  if (#vim.api.nvim_list_wins() == 1) then
-    local w = vim.api.nvim_win_get_width(0)
-    local h = (vim.api.nvim_win_get_height(0) * 2.1)
-    if (h > w) then
-      vim.cmd.split()
-    else
-      vim.cmd.vsplit()
-    end
+  if (vim.system({"git", "rev-parse", "--abbrev-ref", "HEAD"}):wait().code ~= 0) then
+    return vim.notify("not a git repository", vim.log.levels.WARN)
   else
-  end
-  vim.cmd.enew()
-  local bufnr = vim.api.nvim_get_current_buf()
-  local on_exit
-  local function _3_()
-    if (#vim.api.nvim_list_wins() ~= 1) then
-      local buf = vim.fn.bufnr("#")
-      if ((buf ~= -1) and vim.api.nvim_buf_is_valid(buf)) then
-        return vim.api.nvim_win_set_buf(vim.api.nvim_get_current_win(), buf)
+    if (#vim.api.nvim_list_wins() == 1) then
+      local w = vim.api.nvim_win_get_width(0)
+      local h = (vim.api.nvim_win_get_height(0) * 2.1)
+      if (h > w) then
+        vim.cmd.split()
       else
-        return nil
+        vim.cmd.vsplit()
       end
     else
-      if vim.api.nvim_buf_is_valid(bufnr) then
-        return vim.api.nvim_buf_delete(bufnr, {force = true})
+    end
+    vim.cmd.enew()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local on_exit
+    local function _3_()
+      if (#vim.api.nvim_list_wins() ~= 1) then
+        local buf = vim.fn.bufnr("#")
+        if ((buf ~= -1) and vim.api.nvim_buf_is_valid(buf)) then
+          return vim.api.nvim_win_set_buf(vim.api.nvim_get_current_win(), buf)
+        else
+          return nil
+        end
       else
-        return nil
+        if vim.api.nvim_buf_is_valid(bufnr) then
+          return vim.api.nvim_buf_delete(bufnr, {force = true})
+        else
+          return nil
+        end
       end
     end
+    on_exit = _3_
+    vim.fn.termopen("gitu", {on_exit = on_exit})
+    vim.cmd.startinsert()
+    vim.bo[bufnr]["bufhidden"] = "wipe"
+    vim.bo[bufnr]["swapfile"] = false
+    vim.bo[bufnr]["buflisted"] = false
+    return vim.api.nvim_create_autocmd("BufLeave", {buffer = bufnr, once = true, callback = on_exit})
   end
-  on_exit = _3_
-  vim.fn.termopen("gitu", {on_exit = on_exit})
-  vim.cmd.startinsert()
-  vim.bo[bufnr]["bufhidden"] = "wipe"
-  vim.bo[bufnr]["swapfile"] = false
-  vim.bo[bufnr]["buflisted"] = false
-  return vim.api.nvim_create_autocmd("BufLeave", {buffer = bufnr, once = true, callback = on_exit})
 end
 return vim.api.nvim_create_user_command("Gitu", gitu, {})
