f9faf745459740afe9e9b1ad304ea0335eb62cfb
ttak0422 <ttak0422@gmail.com>
2025-07-29 22:46:05 +0900

    fix(avante): move cursor to beginning of line on enter

--- v2/fnl/after/ftplugin/Avante.fnl
+++ v2/fnl/after/ftplugin/Avante.fnl
@@ -3,3 +3,8 @@
 (let [opts {:buffer true :silent true}]
   (each [_ key (ipairs [:i :a :o :A :I :O :s :S :c :C])]
     (vim.keymap.set :n key :<Nop> opts)))
+
+(vim.cmd "normal! 0")
+(vim.api.nvim_create_autocmd :WinEnter
+                             {:buffer 0
+                              :callback #(vim.schedule #(vim.cmd "normal! 0"))})
--- v2/lua/autogen/after/ftplugin/Avante.lua
+++ v2/lua/autogen/after/ftplugin/Avante.lua
@@ -1,7 +1,16 @@
 -- [nfnl] v2/fnl/after/ftplugin/Avante.fnl
 vim.opt_local.conceallevel = 2
-local opts = {buffer = true, silent = true}
-for _, key in ipairs({"i", "a", "o", "A", "I", "O", "s", "S", "c", "C"}) do
-  vim.keymap.set("n", key, "<Nop>", opts)
+do
+  local opts = {buffer = true, silent = true}
+  for _, key in ipairs({"i", "a", "o", "A", "I", "O", "s", "S", "c", "C"}) do
+    vim.keymap.set("n", key, "<Nop>", opts)
+  end
 end
-return nil
+vim.cmd("normal! 0")
+local function _1_()
+  local function _2_()
+    return vim.cmd("normal! 0")
+  end
+  return vim.schedule(_2_)
+end
+return vim.api.nvim_create_autocmd("WinEnter", {buffer = 0, callback = _1_})
