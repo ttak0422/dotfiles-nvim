0002fac213fe2642d526557f57a315134b0d9f94
ttak0422 <ttak0422@gmail.com>
2025-11-16 22:24:16 +0900

    feat(img-clip): Update default options

--- v2/fnl/img-clip.fnl
+++ v2/fnl/img-clip.fnl
@@ -1,17 +1,17 @@
 (local clip (require :img-clip))
 (local default
-       {:dir_path :assets
+       {:dir_path "."
         :extension :png
-        :file_name "%Y%m%d%H%M%S"
+        :file_name "%Y-%m-%d_%H%M%S"
         :use_absolute_path false
-        :relative_to_current_file false
+        :relative_to_current_file true
         ;; template options
         :template :$FILE_PATH
         :url_encode_path false
         :relative_template_path true
         :use_cursor_in_template true
         :insert_mode_after_paste true
-        :prompt_for_file_name true
+        :prompt_for_file_name false
         :show_dir_path_in_prompt false
         ;; base64 options
         :max_base64_size 10
--- v2/lua/autogen/img-clip.lua
+++ v2/lua/autogen/img-clip.lua
@@ -1,5 +1,5 @@
 -- [nfnl] v2/fnl/img-clip.fnl
 local clip = require("img-clip")
-local default = {dir_path = "assets", extension = "png", file_name = "%Y%m%d%H%M%S", template = "$FILE_PATH", relative_template_path = true, use_cursor_in_template = true, insert_mode_after_paste = true, prompt_for_file_name = true, max_base64_size = 10, process_cmd = "", download_images = true, drag_and_drop = {enabled = true, insert_mode = false}, copy_images = false, embed_image_as_base64 = false, relative_to_current_file = false, show_dir_path_in_prompt = false, url_encode_path = false, use_absolute_path = false}
+local default = {dir_path = ".", extension = "png", file_name = "%Y-%m-%d_%H%M%S", relative_to_current_file = true, template = "$FILE_PATH", relative_template_path = true, use_cursor_in_template = true, insert_mode_after_paste = true, max_base64_size = 10, process_cmd = "", download_images = true, drag_and_drop = {enabled = true, insert_mode = false}, copy_images = false, embed_image_as_base64 = false, prompt_for_file_name = false, show_dir_path_in_prompt = false, url_encode_path = false, use_absolute_path = false}
 local filetypes = {markdown = {template = "![$CURSOR]($FILE_PATH)", download_images = false, url_encode_path = false}, html = {template = "<img src=\"$FILE_PATH\" alt=\"$CURSOR\">"}}
 return clip.setup({default = default, filetypes = filetypes})
