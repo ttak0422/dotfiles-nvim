da23758b2b26344ac2c23d463f5ef6025153a167
ttak0422 <ttak0422@gmail.com>
2026-01-06 20:29:23 +0900

    chore(tiny-inline-diagnostic): modify

--- v2/fnl/tiny-inline-diagnostic.fnl
+++ v2/fnl/tiny-inline-diagnostic.fnl
@@ -2,4 +2,11 @@
 
 (diag.setup {:preset :classic
              :transparent_bg true
+             :hi {:error :DiagnosticError
+                  :warn :DiagnosticWarn
+                  :info :DiagnosticInfo
+                  :hint :DiagnosticHint
+                  :arrow :NonText
+                  :background :CursorLine
+                  :mixing_color :None}
              :virt_texts {:priority 2048}})
--- v2/lua/autogen/tiny-inline-diagnostic.lua
+++ v2/lua/autogen/tiny-inline-diagnostic.lua
@@ -1,3 +1,3 @@
 -- [nfnl] v2/fnl/tiny-inline-diagnostic.fnl
 local diag = require("tiny-inline-diagnostic")
-return diag.setup({preset = "classic", transparent_bg = true, virt_texts = {priority = 2048}})
+return diag.setup({preset = "classic", transparent_bg = true, hi = {error = "DiagnosticError", warn = "DiagnosticWarn", info = "DiagnosticInfo", hint = "DiagnosticHint", arrow = "NonText", background = "CursorLine", mixing_color = "None"}, virt_texts = {priority = 2048}})
