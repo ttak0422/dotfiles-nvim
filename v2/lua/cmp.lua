dbcee45c9eb84e9c3225498574c62fde4e1d9ecf
ttak0422 <ttak0422@gmail.com>
2026-05-19 01:03:22 +0900

    fixup

--- v2/lua/cmp.lua
+++ v2/lua/cmp.lua
@@ -33,82 +33,82 @@ lspkind.setup({
 })
 
 cmp.setup({
-		snippet = {
-			expand = function(args)
-				require("luasnip").lsp_expand(args.body)
-			end,
-		},
-		window = {
-			completion = cmp.config.window.bordered(),
-			documentation = cmp.config.window.bordered(),
-		},
-		mapping = cmp.mapping.preset.insert({
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-p>"] = cmp.mapping.select_prev_item(),
-			["<C-n>"] = cmp.mapping.select_next_item(),
-			["<C-e>"] = cmp.mapping.abort(),
-			["<C-y>"] = cmp.mapping.confirm({ select = false }),
-			["<CR>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					if luasnip.expandable() then
-						luasnip.expand()
-					else
-						cmp.confirm({
-							select = true,
-						})
-					end
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<C-y>"] = cmp.mapping.confirm({ select = false }),
+		["<CR>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				if luasnip.expandable() then
+					luasnip.expand()
 				else
-					fallback()
+					cmp.confirm({
+						select = true,
+					})
 				end
-			end),
-			["<Tab>"] = cmp.mapping(function(fallback)
-				if luasnip.locally_jumpable(1) then
-					luasnip.jump(1)
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
-			["<S-Tab>"] = cmp.mapping(function(fallback)
-				if luasnip.locally_jumpable(-1) then
-					luasnip.jump(-1)
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
-			["<C-x>f"] = function()
-				cmp.complete({
-					config = {
-						sources = {
-							{ name = "path" },
-						},
+			else
+				fallback()
+			end
+		end),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if luasnip.locally_jumpable(1) then
+				luasnip.jump(1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<C-x>f"] = function()
+			cmp.complete({
+				config = {
+					sources = {
+						{ name = "path" },
 					},
-				})
-			end,
-			["<C-x>l"] = function()
-				cmp.complete({
-					config = {
-						sources = {
-							{ name = "nvim_lsp" },
-						},
+				},
+			})
+		end,
+		["<C-x>l"] = function()
+			cmp.complete({
+				config = {
+					sources = {
+						{ name = "nvim_lsp" },
 					},
-				})
-			end,
+				},
+			})
+		end,
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	}, {
+		{ name = "buffer" },
+	}),
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol",
+			maxwidth = 50,
+			ellipsis_char = "...",
 		}),
-		sources = cmp.config.sources({
-			{ name = "nvim_lsp" },
-			{ name = "luasnip" },
-		}, {
-			{ name = "buffer" },
-		}),
-		formatting = {
-			format = lspkind.cmp_format({
-				mode = "symbol",
-				maxwidth = 50,
-				ellipsis_char = "...",
-			}),
-		},
-	})
+	},
+})
 
 cmp.setup.cmdline({ "/", "?" }, {
 	mapping = cmp.mapping.preset.cmdline(),
